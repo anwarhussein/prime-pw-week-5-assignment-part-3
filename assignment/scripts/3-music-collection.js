2967b3cc046763dcbca89a814bd33c8c01e4e8cf
Anwar Hussein <anwarhussein2353@gmail.com>
2021-08-10 15:40:44 -0500

    third commit

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -22,9 +22,24 @@ console.log(addToCollection('Indha deeraley', 'Saada Ali', 1991));
 //(Feel free to share your musical interests, or make stuff up. Totally fine either way.)
 //Console.log each album as added using the returned value.
 //After all are added, console.log the `collection` array.
+
 console.log(addToCollection('Boomba train','nameless',2003));
 console.log(addToCollection('zilizopendwa','Saadia Abdo',1988));
 console.log(addToCollection('Rabaaso', 'Abdi Diini',2009));
 console.log(addToCollection('nipekitu', 'nameless',2000));
 console.log(addToCollection('Mikasi','Ferooz', 2004));
 console.log(addToCollection('sema basi', 'Mb dogg', 2006));
+
+console.log(collection);
+
+// Add a function named showCollection. This function should:
+// Take in an array parameter. (This allows it to be reused to show any collection,
+// like the results from the find or search.)
+// Console.log the number of items in the array.
+// Loop over the array and console.log each album's information
+// formatted like: TITLE by ARTIST, published in YEAR.
+
+function showCollection(array){
+  console.log(array.length);
+}
+showCollection(collection);
