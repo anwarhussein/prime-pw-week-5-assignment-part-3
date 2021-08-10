0647e0ca824a8a7193ae1326dcab3e1a2b6b014a
Anwar Hussein <anwarhussein2353@gmail.com>
2021-08-10 09:38:54 -0500

    first commit

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,23 @@
 console.log('***** Music Collection *****')
+// Create a variable `collection` that starts as an empty array.
+// Add a function named `addToCollection`. This function should:
+// Take in the album's `title`, `artist`, `yearPublished` as input parameters
+// Create a new object having the above properties
+// Add the new object to the end of the `collection` array
+// Return the newly created object
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+  const albumObj = {
+    title: 'indha deeraley',
+    artist:'Saada Ali',
+    yearPublished: 1991
+}
+  collection.push(albumObj);
+  return albumObj;
+}
+//Test the `addToCollection` function:
+//Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years.
+//(Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+//Console.log each album as added using the returned value.
+//After all are added, console.log the `collection` array.
